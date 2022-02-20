f2fa2c668c76d14a8d2847c6a8c54199e6840bc2
wowermalwina <wowermalwina@gmail.com>
2022-02-20 20:13:02 +0000

    created weather component, updated favicon, deleted old files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Search/Search.jsx
+++
@@ -1,7 +0,0 @@
-import "./Search.scss";
-
-function Search() {
-  return <div className="Search"></div>;
-}
-
-export default Search;
